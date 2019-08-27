b7426c49021bd751626fc9e50a0bbe2ef5779aa0
Noam_Levy <noamlv@mta.ac.il>
2019-08-27 17:12:29 +0300

    first commit ex 2

---
+++ src/sample/Controller.java
@@ -0,0 +1,120 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Controller {
+
+
+    @FXML
+    private MenuItem NewMenuItem;
+
+    @FXML
+    private MenuItem loadFromXMLMenuItem;
+
+    @FXML
+    private MenuItem changeUserNameMenuItem;
+
+    @FXML
+    private MenuItem changeRepoMenuItem;
+
+    @FXML
+    private MenuItem showCommitMenuItem;
+
+    @FXML
+    private MenuItem openChangesMenuItem;
+
+    @FXML
+    private MenuItem newCommitMenuItem;
+
+    @FXML
+    private MenuItem showBranchesMenuItem;
+
+    @FXML
+    private MenuItem newBranchMenuItem;
+
+    @FXML
+    private MenuItem deleteBranchMenuItem;
+
+    @FXML
+    private MenuItem checkoutBranchMenuItem;
+
+    @FXML
+    private MenuItem resetBranchMenuItem;
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+
+    @FXML
+    void onChageRepo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onChageUserName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCheckoutBranch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCommit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteBranch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLoadFromXML(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNewBranch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNewRepo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onResetBranch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShowBranches(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShowCommit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShowOpenChanges(ActionEvent event) {
+
+    }
+}
---
+++ src/sample/MessageBox.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class MessageBox {
+
+    private static final int MIN_WIDTH = 250;
+    private static final int SPACING_CHILDRENS = 10;
+
+    public static void display(String i_Title, String i_Message){
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setTitle(i_Title);
+        stage.setMinWidth(MIN_WIDTH);
+        Label label = new Label(i_Message);
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> stage.close());
+
+        VBox layout = new VBox(SPACING_CHILDRENS);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(label, closeButton);
+        Scene scene = new Scene(layout);
+        stage.setScene(scene);
+        stage.showAndWait();
+    }
+}
